3bb7899cb3b54036ebc01b19cca754cb4d7464fb
wesamustafa <wesamustafa@gmail.com>
2021-06-12 11:58:20 +0200

    added scroll top button

--- js/main.js
+++ js/main.js
@@ -9,4 +9,10 @@ menu.onclick = () =>{
 window.onscroll = ()=>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+    // scroll top button
+    if(window.scrollY > 60){
+        document.querySelector("#scroll-top").classList.add("active");
+    }else{
+        document.querySelector("#scroll-top").classList.remove("active");
+    }
 }
